630e95ecf7d9b862ea591cc8ab593897d5910ba3
cwood89 <cwood89@mac.com>
2020-05-11 14:10:44 -0400

    added ingredients to modal

--- assets/src/products.js
+++ assets/src/products.js
@@ -1,4 +1,4 @@
-function Product(name, popularity, image, description) {
+function Product(name, popularity, image, description, ingredients) {
   this.id = name.replace(/\s/g, '');
   this.name = name;
   this.priceSmall = 4.99;
@@ -7,32 +7,104 @@ function Product(name, popularity, image, description) {
   this.sizeLarge = "12oz";
   this.popularity = popularity; // 1-10 scale
   this.image = image;
-  this.description = description
+  this.description = description;
+  this.ingredients = ingredients
 }
 let products = [
-  new Product("Super Berry", 6, "images/products/superberry.png", "Super Berry is exactly what you imagine, the perfect blend of sweet and sour berries."),
+  new Product(
+    "Super Berry",
+    6,
+    "images/products/superberry.png",
+    "Super Berry is exactly what you imagine, the perfect blend of sweet and sour berries.",
+    "Wild Blueberries, Organic Strawberries, Organic Blueberries, Organic Cane Sugar, Cinnamon Apple Pectin, Ascorbic Acid (Vitamin C), Citric Acid."
+  ),
 
-  new Product("Strawberry", 8, "images/products/Strawberry.png", "Strawberry jelly is the stuff of childhood dreams. Sunday afternoon tea is never right without it. It is an essential part of every pantry."),
+  new Product(
+    "Strawberry",
+    8,
+    "images/products/Strawberry.png",
+    "Strawberry jelly is the stuff of childhood dreams. Sunday afternoon tea is never right without it. It is an essential part of every pantry.",
+    "Water, Organic Strawberries, Organic Cane Sugar, Citric Acid, Natural Flavor, Organic Fruit And Vegetable Juice [Color]"
+  ),
 
-  new Product("Raspberry", 5, "images/products/raspberry.png", "The intense raspberry flavor of this jam makes it a longtime favorite. If you're not going to eat raspberries fresh out of hand by the fistful, this jam is the next best thing."),
+  new Product(
+    "Raspberry",
+    5,
+    "images/products/raspberry.png",
+    "The intense raspberry flavor of this jam makes it a longtime favorite. If you're not going to eat raspberries fresh out of hand by the fistful, this jam is the next best thing.",
+    "Organic Cane Sugar, Organic Raspberries, Fruit Pectin, Citric Acid."
+  ),
 
-  new Product("Mixed Berry", 6, "images/products/mixberry.png", "The perfect choice for the undecided, this jam has a bit of everything."),
+  new Product(
+    "Mixed Berry",
+    6,
+    "images/products/mixberry.png",
+    "The perfect choice for the undecided, this jam has a bit of everything.",
+    "Strawberries (Organic), Raspberries (Organic), Blueberries (Organic), Grape Puree (Organic), Water, Lemon Juice Concentrate (Organic), Dried Sugar Cane Syrup (Organic), Fruit Pectin."
+  ),
 
-  new Product("Lemon", 4, "images/products/lemon.png", "Jazz up a biscuit, English muffin, or slice of toast with a spoonful of Lemon Jelly. You know what they say (or what they should say): When life give you lemons, make Lemon Jelly!"),
+  new Product(
+    "Lemon",
+    4,
+    "images/products/lemon.png",
+    "Jazz up a biscuit, English muffin, or slice of toast with a spoonful of Lemon Jelly. You know what they say (or what they should say): When life give you lemons, make Lemon Jelly!",
+    "Meyer Lemons, Fresh Meyer Lemon Juice, Organic Cane Juice"
+  ),
 
-  new Product("Grape Jelly", 9, "images/products/grapejelly.png", " America's favorite. Every jar starts with one pound of grapes. For delicious, one‑of‑a‑kind taste with no artificial flavors or colors."),
+  new Product(
+    "Grape Jelly",
+    9,
+    "images/products/grapejelly.png", 
+    "America's favorite. Every jar starts with one pound of grapes. For delicious, one‑of‑a‑kind taste with no artificial flavors or colors.",
+    "Organic Sugar, Water, Organic Concord Grape Juice Concentrate, Citric Acid, Fruit Pectin."
+  ),
 
-  new Product("Ginger Cardamom Pear Butter", 3, "images/products/Ginger-Cardamom-Pear-Butter.png", "Spread this fruity butter on muffins or whole grain baguettes and savor the flavors of pear and ginger."),
+  new Product(
+    "Ginger Cardamom Pear Butter",
+    3,
+    "images/products/Ginger-Cardamom-Pear-Butter.png",
+    "Spread this fruity butter on muffins or whole grain baguettes and savor the flavors of pear and ginger.",
+    "Bartlett Pears, Organic Evaporated Cane Juice, Cardamom, Ginger, Lemon Juice, Salt"
+  ),
 
-  new Product("Cinnamon Blueberry", 4, "images/products/cinnamonblueberry.png", "Many people might think warm spices belong to the winter holidays, but I find them most enjoyable paired with summer fruit.The hint of cinnamon perfectly balances the sweet and tart flavor of blueberries."),
+  new Product(
+    "Cinnamon Blueberry",
+    4,
+    "images/products/cinnamonblueberry.png",
+    "Many people might think warm spices belong to the winter holidays, but I find them most enjoyable paired with summer fruit.The hint of cinnamon perfectly balances the sweet and tart flavor of blueberries.",
+    "Organic Wild Blueberries, Organic Cane Sugar, Cinnamon Apple Pectin, Ascorbic Acid (Vitamin C), Citric Acid."
+  ),
 
-  new Product("Cinnamon Spiced Triple Berry Jam", 6, "images/products/Cinnamon-Spiced-Triple-Berry-Jam.png", "On waffles or in cocktails, this beautiful spiced triple berry jam is a delicious addition to your pantry."),
+  new Product(
+    "Cinnamon Spiced Triple Berry Jam",
+    6,
+    "images/products/Cinnamon-Spiced-Triple-Berry-Jam.png",
+    "On waffles or in cocktails, this beautiful spiced triple berry jam is a delicious addition to your pantry.",
+    "Berry Mix- (Organic Strawberries, Organic Blackberries, Organic Blueberries), Water, Organic Sugar, Fruit Pectin, Calcium Chloride, Citric Acid, Cinnamon, Allspice, Nutmeg"
+  ),
 
-  new Product("Blueberry", 7, "images/products/blueberry.png", "The health benefits from eating blueberries are so great that we decided to preserve them as a jam, and honestly we can't pick what we like it on the most."),
+  new Product(
+    "Blueberry",
+    7,
+    "images/products/blueberry.png",
+    "The health benefits from eating blueberries are so great that we decided to preserve them as a jam, and honestly we can't pick what we like it on the most.",
+    "Organic Wild Blueberries, Organic Cane Sugar, Apple Pectin"
+  ),
 
-  new Product("Blackberry", 7, "images/products/Blackberry.png", " Savor the tart, sweet flavor of fresh summer blackberries all year round with this Blackberry Jelly recipe. Can complement any dessert."),
+  new Product(
+    "Blackberry",
+    7,
+    "images/products/Blackberry.png", " Savor the tart, sweet flavor of fresh summer blackberries all year round with this Blackberry Jelly recipe. Can complement any dessert.",
+    "Organic Seedless Blackberry Puree, Organic Cane Sugar, Fruit Pectin"
+  ),
 
-  new Product("Apple Butter", 10, "images/products/applebutter.png", "This homemade Apple Butter is smooth, creamy, and velvety. Super healthy, delicious, and perfect for spreading on toast, muffins, pancakes etc.")
+  new Product(
+    "Apple Butter",
+    10,
+    "images/products/applebutter.png",
+    "This homemade Apple Butter is smooth, creamy, and velvety. Super healthy, delicious, and perfect for spreading on toast, muffins, pancakes etc.",
+    "Apples, Cinnamon, Allspice, Brown Sugar, Honey"
+  )
 ];
 
   let row1 = $("#row-1")
@@ -54,7 +126,7 @@ let products = [
   <div class="modal fade" id="${product.id}" tabindex="-1" role="dialog" aria-labelledby="${product.id}modal" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header" style="background-color:#ff5c5c; color:white;">
           <h5 class="modal-title" id="exampleModalLongTitle">Product Information</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
@@ -93,6 +165,10 @@ let products = [
                     <p>
                     ${product.description}
                     </p>
+                    <h4>Ingredients</h4>
+                    <p style="margin-bottom: -60px;">
+                    ${product.ingredients}
+                    </p>
                     </div>
                 </div>
     
@@ -101,8 +177,8 @@ let products = [
           </div>
         </section><!-- End Portfolio Details Section -->
         </div>
-        <div class="modal-footer">
-          <button type="button"  id="modal-button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <div class="modal-footer" style="background-color:#161E67">
+          <button type="button"  id="modal-button" class="btn btn-primary" data-dismiss="modal">Close</button>
         </div>
       </div>
     </div>
